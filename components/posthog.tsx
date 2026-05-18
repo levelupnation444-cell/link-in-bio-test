1cc38576be57f56448d4afb4ab8cb2be950f9e2a
Ksawery <47505116+ksawery29@users.noreply.github.com>
2026-05-18 22:34:55 +0200

    make posthog work without cookies (for eu compliance)

--- components/posthog.tsx
+++ components/posthog.tsx
@@ -13,6 +13,7 @@ export function PostHog() {
     posthog.init(POSTHOG_KEY, {
       api_host: POSTHOG_HOST,
       capture_pageview: "history_change",
+      persistence: 'memory'
     });
   }, []);
 
